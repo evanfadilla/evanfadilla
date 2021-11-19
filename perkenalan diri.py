d213de36076aeae2f67c028b920b792baffcd176
evanfadilla <94690441+evanfadilla@users.noreply.github.com>
2021-11-19 19:42:00 +0700

    Add files via upload

---
+++ perkenalan diri.py
@@ -0,0 +1,15 @@
+nama = "Evan Fadilla Setiawan" #tipe datanya string, karena valuenya berisi huruf dan terdapat tanda petik dua diatas
+nim = 212410102062 #tipe datanya integer, karena valuenya berisi angka
+umur = "19 tahun" #tipe datanya string, karena valuenya terdapat tanda petik dua diatas
+tinggi_badan = 174 #tipe datanya integer, karena value nya berisi angka
+berat_badan = 74.5 #tipe datanya float, karena value nya berisi angka desimal
+jomblo = True #tipe datanya boolean, karena value nya berisi True
+
+
+
+print(nama)
+print(nim)
+print(umur)
+print(tinggi_badan)
+print(berat_badan)
+print(jomblo)
